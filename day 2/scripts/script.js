9b1e0f59bdbc940c72c2a5fe6c59263827557b1f
vinod1490 <53248792+vinod1490@users.noreply.github.com>
2020-07-23 19:38:23 +0530

    Add files via upload

---
+++ day 2/scripts/script.js
@@ -0,0 +1,4 @@
+//console.log("hello");
+let name =prompt("Enter your name");
+
+console.log(name);
---
+++ day 2/scripts/script2.js
@@ -0,0 +1,8 @@
+let ans = prompt("are you 21+ ?");
+
+if(ans=="yes"){
+    console.log("You can Drink");
+}
+else{
+    console.log("you can't Drink");
+}
